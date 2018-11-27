84e6a5efccfe8974bffba27d5f1ee7b0580a7fc2
nobertnn <45212856+nobertnn@users.noreply.github.com>
2018-11-27 17:54:30 +0100

    Aufgabe 3 und 4

--- PC-17/CMain.java
+++ PC-17/CMain.java
@@ -1,14 +1,36 @@
-public class CMain { 
-    public static void main(String[] p_args) 
-    { 
+/*
+ * @cond LICENSE
+ * ######################################################################################
+ * # Hausaufgabe3 27/11/2018                                                            #
+ * #                                                                                    #
+ * # Authored by Mengjie Zou/Jincheng Hu                                                #
+ * ######################################################################################
+ * @endcond
+ */
+
+
+/**
+ * Test Mainmethode für hierarchische Typenvererbung
+ * demonstriert Vewrwendung von instanceof und Casting bei Klassen mit Vererbung
+ */
+
+public class CMain
+{
  
-       IFahrzeug car = new CAuto(); 
-       IFahrzeug suv = new CSuv();  
-        car.beschleunige(1);  
-        car.bremse(2); 
-        car.schalte(3); 
-        suv.beschleunige(4); 
-         suv.bremse(5); 
-         suv.schalte(6); 
-    }  
-} 
+
+    public static void main( String[] args )
+        {
+        CAuto auto1 = new CSuv("rot", "golf", 6, 180, false);
+        CAuto auto2 = new CAuto("gelb", "VW", 6, 220);
+        IFahrzeug fox = auto2;
+        IFahrzeug jeep;
+        jeep = auto1;
+        jeep.schalte(5);
+        System.out.println(auto1.getGang());
+        fox.schalte(3);
+        System.out.println(auto2.getGang());
+        
+
+     // nutze diese Methode für die weiteren Teile der Aufgabe
+    }
+}
